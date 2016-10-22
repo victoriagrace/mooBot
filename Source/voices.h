f403929eb904a498aa77c97248fdc6e8ad5df1cd
Victoria Grace <victoriagrace@DN51t5i3.SUNet>
2016-10-22 15:56:23 -0700

    added files

---
+++ Source/voices.h
@@ -0,0 +1,18 @@
+/*
+  ==============================================================================
+
+    voices.h
+    Created: 22 Oct 2016 3:52:34pm
+    Author:  Victoria Grace
+
+  ==============================================================================
+*/
+
+#ifndef VOICES_H_INCLUDED
+#define VOICES_H_INCLUDED
+
+
+
+
+
+#endif  // VOICES_H_INCLUDED
